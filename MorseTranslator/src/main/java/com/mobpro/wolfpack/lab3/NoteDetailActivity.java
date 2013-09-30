ce64ab257094e7ab2f380ffaa80c572e9c7cbf37
Rachel Biniaz <rbiniaz@gmail.com>
2013-09-30 13:01:43 -0400

    work done in class

--- MorseTranslator/src/main/java/com/mobpro/wolfpack/lab3/NoteDetailActivity.java
+++ MorseTranslator/src/main/java/com/mobpro/wolfpack/lab3/NoteDetailActivity.java
@@ -13,10 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,12 +25,61 @@ public class NoteDetailActivity extends Activity implements SurfaceHolder.Callba
 
     public Camera mCamera;
     public SurfaceHolder mHolder;
-    HashMap<String,String> morse;
+    public static long unitTime = 200;
+
+    static long dot = unitTime;
+    static long dash = unitTime*3;
+    static long wait = unitTime;
+    static long endLetter = unitTime*3;
+
+    public static HashMap<String, long[]> TIMES = new HashMap<String, long[]>(){{
+        put("a",new long[]{dot,wait,dash,endLetter});
+        put("b",new long[] {dash, wait,dot,wait,dot,wait,dot, endLetter});
+        put("c", new long[] {dash, wait,dot,wait,dash,wait,dot,endLetter});
+        put("d", new long[] {dash, wait, dot, wait,dot,endLetter});
+        put("e",new long[] {dot, endLetter});
+        put("f", new long[]{dot, wait, dot,wait,dash,wait,dot, endLetter});
+        put("g",new long[] {dash,wait,dash,wait,dot,endLetter});
+        put("h",new long[] {dot,wait,dot,wait,dot,wait,dot,endLetter});
+        put("i",new long[] {dot, wait,dot,endLetter});
+        put("j", new long[] {dot,wait,dash,wait,dash,wait,dash,endLetter});
+        put("k", new long[] {dash,wait,dot,wait,dash,endLetter});
+        put("l", new long[] {dot, wait,dash,wait,dot,wait,dot,endLetter});
+        put("m", new long[] {dash, wait,dash, endLetter});
+        put("n", new long[] {dash, wait,dot,endLetter});
+        put("n",new long[] {dash,wait,dot,endLetter});
+        put("o", new long[] {dash, wait,dash,wait,dash,endLetter});
+        put("p", new long[] {dot,wait,dash,wait,dash,wait,dot,endLetter});
+        put("q", new long[] {dash, wait,dash,wait,dot,wait,dash,endLetter});
+        put("r",new long[] {dot,wait,dash,wait,dot,endLetter});
+        put("s",new long[]{dot,wait,dot,wait,dot,endLetter});
+        put("t",new long[]{dash,endLetter});
+        put("u",new long[] {dot,wait,dot,wait,dash,endLetter});
+        put("v", new long[]{dot,wait,dot,wait,dot,wait,dash,endLetter});
+        put("w",new long[] {dot,wait,dash,wait,dash,endLetter});
+        put("x", new long[]{dash,wait,dot,wait,dot,wait,dash,endLetter});
+        put("y",new long[] {dash,wait,dot,wait,dash,wait, dash,endLetter});
+        put("z", new long[]{dash, wait,dash,wait,dot,wait,dot,endLetter});
+    }
+    };
+
+    public static HashMap<String, String> MORSE = new HashMap<String, String>(){{
+        put("a","*-");put("b","-***");put("c", "-*-*");put("d", "-**");
+        put("e", "*");put("f","**-*");put("g","--*");
+        put("h","****");put("i","**");put("j","*---");put("k","-*-");
+        put("l","*-**");put("m","--");put("n","-*");put("n","-*");
+        put("o","---");put("p","*--*"); put("q", "--*-");put("r","*-*");
+        put("s","***");put("t","-");put("u","**-");put("v", "***-");
+        put("w","*--");put("x", "-**-");put("y","-*--"); put("z","--**");
+
+    }};
+
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
@@ -54,16 +100,14 @@ public class NoteDetailActivity extends Activity implements SurfaceHolder.Callba
             Log.e("IOException", e.getMessage());
         }
 
-        //Morse-translation related code
-        this.morse = getMorse();
-
 
         final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         vibrateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //vibrate
-                vibrator.vibrate(1000);
+                ArrayList morse = readMorse("hello");
+
             }
         });
 
@@ -79,20 +123,7 @@ public class NoteDetailActivity extends Activity implements SurfaceHolder.Callba
     }
 
     private void flashForTime(int t){
-        //flashes
-        Camera.Parameters params = mCamera.getParameters();
-        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-        mCamera.setParameters(params);
-        mCamera.startPreview();
-        try {
-            wait(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-        mCamera.setParameters(params);
-        mCamera.stopPreview();
-        //new MyAsyncTask().execute();
+        new MyAsyncTask().execute();
 
     }
 
@@ -116,25 +147,16 @@ public class NoteDetailActivity extends Activity implements SurfaceHolder.Callba
         mHolder = null;
     }
 
-    public HashMap<String, String> getMorse() {
-        return new HashMap<String, String>(){{
-            put("a","*-");put("b","-***");put("c", "-*-*");put("d", "-**");
-            put("d","-**");put("e","*");put("f","**-*");put("g","--*");
-            put("h","****");put("i","**");put("j","*---");put("k","-*-");
-            put("l","*-**");put("m","--");put("n","-*");put("n","-*");
-            put("o","---");put("p","*--*"); put("q", "--*-");put("r","*-*");
-            put("s","***");put("t","-");put("u","**-");put("v", "***-");
-            put("w","*--");put("x", "-**-");put("y","-*--"); put("z","--**");
 
-        }};
-    }
 
+
+    // _ denotes wait
     public ArrayList<String> englishToMorse(String english){
         String[] characters = english.split("");
         ArrayList<String> morseCode = new ArrayList<String>();
-        for (int i = 0; i == english.length(); i++){
-            if(this.morse.containsKey(characters[i])){
-                morseCode.add(morse.get(characters[i]));
+        for (int i = 0; i < english.length(); i++){
+            if(this.MORSE.containsKey(characters[i])){
+                morseCode.add(MORSE.get(characters[i]));
                 morseCode.add("___");
             }
             else if (characters[i] == " "){
@@ -145,7 +167,18 @@ public class NoteDetailActivity extends Activity implements SurfaceHolder.Callba
         return morseCode;
     }
 
-    public void readMorse(ArrayList<String> morseInput){
+    public ArrayList<Long> readMorse(String english){
+        String[] characters = english.split("");
+        ArrayList<Long> times = new ArrayList<Long>();
+        for (int i = 0; i < english.length(); i++){
+            if (this.TIMES.containsKey(characters[i])){
+//                for (int j=0; j<TIMES.get())
+                for (long j : TIMES.get(characters[i])){
+                    times.add(j);
+                }
+            }
+        }
+        return times;
 
     }
 
@@ -153,21 +186,53 @@ public class NoteDetailActivity extends Activity implements SurfaceHolder.Callba
         String[] t = s.split("");
         return null;
     }
-    /*
-    private class MyAsyncTask extends AsyncTask {
+
+    private class MyAsyncTask extends AsyncTask{
+
 
         @Override
-        protected Object doInBackground(Object[] objects) {
-            try {
-                wait(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        protected void onPreExecute() {
+            Log.d("wolf", "onPreExecute");
+            Camera.Parameters params = mCamera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            mCamera.setParameters(params);
+            mCamera.startPreview();
+
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            Log.d("wolf", "onPostExecute");
+
             Camera.Parameters params = mCamera.getParameters();
             params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
             mCamera.setParameters(params);
             mCamera.stopPreview();
-           return null;
         }
-    }*/
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            Log.d("wolf", "doInBackground");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onCancelled() {
+            Log.d("wolf", "onCancelled");
+
+            Camera.Parameters params = mCamera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            mCamera.setParameters(params);
+            mCamera.stopPreview();
+        }
+    }
+
 }
